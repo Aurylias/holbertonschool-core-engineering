25923e002e8447885b94242ce0b616e6cef56624
Aurylias <brycevermorel@gmail.com>
2026-08-31 15:46:44 +0200

    fix: Remade whole code for unspecified contraint in task

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-numList = []
-for num in range(1, 100):
-    if str(num)[::-1] in numList or "{:02d}".format(num)[0] == \
-"{:02d}".format(num)[1]:
-        pass
-    else:
-        numList.append("{:02d}".format(num))
-print(", ".join(numList))
+for firstDigit in range(10):
+    for secondDigit in range(firstDigit + 1, 10):
+        if firstDigit == 8 and secondDigit == 9:
+            print("{}{}".format(firstDigit, secondDigit))
+        else:
+            print("{}{}".format(firstDigit, secondDigit), end=", ")
