cd58ed747fdad8caa3a84380cedbfd329aff1b66
Aurylias <brycevermorel@gmail.com>
2026-08-31 14:57:33 +0200

    fix: Use .format as ask in the taks

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-for num in range(100):
-    if num == 99:
-        print(f"{num}")
+for num in range(0, 100):
+    if num < 99:
+        print("{:02d}".format(num), end=", ")
     else:
-        print(f"{num:02d}, ", end="")
+        print("{:02d}".format(num))
