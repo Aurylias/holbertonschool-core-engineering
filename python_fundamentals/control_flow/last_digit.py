2af3f97836308999e837a1b1fddbad68f4449c8f
Aurylias <brycevermorel@gmail.com>
2026-08-31 14:17:20 +0200

    fix: Remade logic for digit

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
-absNumber = -number if number < 0 else number
-neg = True if number < 0 else False
-digit = absNumber % 10
-if digit > 5:
-    if neg:
-        print(f"Last digit of {number} is -{digit} and is greater than 5")
-    else:
-        print(f"Last digit of {number} is {digit} and is greater than 5")
-elif digit < 6 and digit != 0:
-    if neg:
-        print(f"Last digit of {number} is -{digit}\
-              and is less than 6 and not zero")
-    else:
-        print(f"Last digit of {number} is {digit}\
-              and is less than 6 and not zero")
+digit = -(abs(number) % 10) if number < 0 else number % 10
+
+if (digit > 5):
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+elif (digit < 6 and digit != 0):
+    print(
+        f"Last digit of {number} is {digit} \
+and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {digit} and is zero")
+    print(f"Last digit of {number} is {digit} and is 0")
