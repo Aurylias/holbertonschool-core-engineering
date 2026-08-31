c892455ff5c387ee271400471747925131f04c48
Aurylias <brycevermorel@gmail.com>
2026-08-31 16:50:47 +0200

    fix: Remade whole function

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 def uppercase(str):
+    finalString = ""
     for char in str:
-        if ord(char) >= 65 and ord(char) <= 90:
-            print(f"{char}".format(), end="")
+        ascii = ord(char)
+        if ascii >= 97 and ascii <= 122:
+            finalString += chr(ascii - 32)
         else:
-            print(f"{chr(ord(char) - 32)}".format(), end="")
+            finalString += char
+
+    print("{}".format(finalString))
