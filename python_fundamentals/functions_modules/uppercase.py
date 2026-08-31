7846251c1bd4b1abce52b4881792037f370b2852
Aurylias <brycevermorel@gmail.com>
2026-08-31 16:27:04 +0200

    Initial commit

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 65 and ord(char) <= 90:
+            print(char, end="")
+        else:
+            print(chr(ord(char) - 32), end="")
+    print("\n")
