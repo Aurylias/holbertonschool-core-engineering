54ba0de1ff77d54658c9b5f32a8193d00a49e8d6
Aurylias <brycevermorel@gmail.com>
2026-09-01 11:48:10 +0200

    Initial commit

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from calculator_1.py import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {add(a, b)}".format())
+    print(f"{a} - {b} = {sub(a, b)}".format())
+    print(f"{a} * {b} = {mul(a, b)}".format())
+    print(f"{a} / {b} = {div(a, b)}".format())
