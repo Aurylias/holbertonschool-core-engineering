5eb333070e05c15189f8b825c57ebaadc208d07a
Aurylias <brycevermorel@gmail.com>
2026-09-01 08:32:33 +0200

    fix: remove newline at end of print

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     """Print the last digit of a number"""
     number = -number if number < 0 else number
     digit = number % 10
-    print(digit)
+    print(digit, end="")
     return digit
