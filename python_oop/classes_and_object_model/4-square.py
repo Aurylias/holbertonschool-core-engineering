3bf5837b1ad41155204dc0614626bd3a2f9e165d
Aurylias <brycevermorel@gmail.com>
2026-09-07 12:52:13 +0200

    fix: change var name for if in setSize

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -21,7 +21,7 @@ class Square:
         return self.__size
     
     def setSize(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
