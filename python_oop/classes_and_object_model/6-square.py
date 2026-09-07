cbdddef113ec2588629aa25f01edb647e42d401d
Aurylias <brycevermorel@gmail.com>
2026-09-07 16:29:09 +0200

    fix: add condition to check if tuple value are int

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -16,7 +16,9 @@ class Square:
         except IndexError:
             position = (-1, -1)
         if not isinstance(position, tuple) or len(position) < 2 or \
-         (position[0] < 0 or position[1] < 0):
+           (position[0] < 0 or position[1] < 0) \
+           or not isinstance(position[0], int) \
+           or not isinstance(position[1], int):
             print("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
