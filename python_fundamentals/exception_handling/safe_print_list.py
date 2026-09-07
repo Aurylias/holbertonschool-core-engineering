67f98c0fd114d4c902eaa3bf9c4a20c0e0dca973
Aurylias <brycevermorel@gmail.com>
2026-09-07 09:39:04 +0200

    fix: now return the number of  printed elements

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 def safe_print_list(my_list=[], x=0):
     """Safely print a list"""
+    total = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
+            total += 1
     except:
         print("Given lenght is bigger than the len of the list")
+    return total
