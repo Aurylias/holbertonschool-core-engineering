c958a48856805e69e0ba4fd8182a76b3fda438cc
Aurylias <brycevermorel@gmail.com>
2026-09-07 11:52:42 +0200

    Initial commit

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Represents a square"""
+
+
+class Square:
+    """Class for a square, do noting at the moment"""
+    def __init__(self, size):
+        self.__size = size
