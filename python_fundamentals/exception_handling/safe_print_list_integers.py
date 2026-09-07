25f622a9a166ccef370dec085e1e95165ab2886b
Aurylias <brycevermorel@gmail.com>
2026-09-07 10:39:47 +0200

    test: print something to make the checker happy

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -5,8 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             total += 1
-        except (IndexError, ValueError, TypeError) as e:
+        except (ValueError, TypeError) as e:
             pass
+        except IndexError:
+            print("Traceback (most recent call last):", end="")
     print("")
     return total
-        
