f6377a3fd30ed8782e515fa7dfd9c205005cb423
Aurylias <brycevermorel@gmail.com>
2026-09-07 16:57:50 +0200

    Initial commit

---
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Represents a rectangle"""
+
+
+class Rectangle:
+    """Class for a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Class constructor"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be an integer")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be an integer")
+        self.__height = value
