66bf12cafddcb6ce52bef8465b358e6935ce53a6
Aurylias <brycevermorel@gmail.com>
2026-09-07 09:34:04 +0200

    fix: for logic to print list elements

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     """Safely print a list"""
     try:
-        for elem in range(x):
-            print(str(elem), end="")
+        for i in range(x):
+            print(str(my_list[i]), end="")
     except:
         print("Given lenght is bigger than the len of the list")
