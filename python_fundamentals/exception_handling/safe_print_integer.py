251fe90b8f2aa204eddb8b60a64a0290289c8f0d
Aurylias <brycevermorel@gmail.com>
2026-09-07 10:09:44 +0200

    fix: add the format as asked in the task and changed logic

--- python_fundamentals/exception_handling/safe_print_integer.py
+++ python_fundamentals/exception_handling/safe_print_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 def safe_print_integer(value):
     try:
-        if value.is_Integer():
-            print(value)
-        else:
-            raise TypeError()
-    except TypeError:
+        print("{:d}".format(value))
+    except ValueError:
         return False
