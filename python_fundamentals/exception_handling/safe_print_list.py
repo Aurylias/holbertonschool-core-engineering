cccc79fa42be6f7f107206662ae8aa60e1909fed
Aurylias <brycevermorel@gmail.com>
2026-09-07 09:42:26 +0200

    fix: made the except do nothing

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             total += 1
     except:
-        print("Given lenght is bigger than the len of the list")
+        pass
     print("")
     return total
