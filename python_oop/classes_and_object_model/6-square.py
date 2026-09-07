51caf294e478f45fa2934105c5f06539867c0aca
Aurylias <brycevermorel@gmail.com>
2026-09-07 16:18:23 +0200

    fix: check if the second index of position exist or not

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -11,8 +11,12 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        if not isinstance(position, tuple) or (len(position) < 2 and 
-                         (position[0] < 0 or position[1] < 0)):
+        try:
+            print(position[1])
+        except IndexError:
+            position = (position[0], 0)
+        if not isinstance(position, tuple) or len(position) < 2 or \
+         (position[0] < 0 or position[1] < 0):
             print("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
