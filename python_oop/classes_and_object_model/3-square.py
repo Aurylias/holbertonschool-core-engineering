9e83eb5bdcc7757468aae3edcdcb5a06877565c7
Aurylias <brycevermorel@gmail.com>
2026-09-07 12:35:12 +0200

    Initial commit

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Represents a square"""
+
+
+class Square:
+    """Class for a square, do noting at the moment"""
+    def __init__(self, size=0):
+        """Instantiation with a given size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square"""
+        return self.size * self.size()
