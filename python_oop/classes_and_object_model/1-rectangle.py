114aa2192a865c43c8f8333bed88b751a59d7e79
Aurylias <brycevermorel@gmail.com>
2026-09-07 17:01:51 +0200

    fix: wrong output in raise

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         elif height < 0:
-            raise ValueError("height must be an integer")
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
