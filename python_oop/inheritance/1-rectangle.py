16478225aa32e80c3049fc6ea30be8f91b9ebb1c
Aurylias <brycevermorel@gmail.com>
2026-09-08 10:31:58 +0200

    fix: Remove last change

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Class that represente a square"""
-    def __init__(self, width=1, height=1) -> None:
+    def __init__(self, width=1, height=1):
         """Constructor"""
         self.integer_validator("width", width)
         self.__width = width
