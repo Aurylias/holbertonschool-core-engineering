4b88e10455742d1aeb2937aa03293394881a58a2
Aurylias <brycevermorel@gmail.com>
2026-09-08 10:44:10 +0200

    Initial commit

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""A module that handle the base for geometric shapes"""
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class that represente a square"""
+    def __init__(self, width=1, height=1):
+        """Constructor"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height   
+
+    def __str__():
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area():
+        """Calculate the area of the rectangle"""
+        return self.__width * self.__height
