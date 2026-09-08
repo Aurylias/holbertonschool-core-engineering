0145c86dc9086fff373a5ec0e1aee46afa7bd654
Aurylias <brycevermorel@gmail.com>
2026-09-08 10:22:16 +0200

    fix: removed whole BaseGeometry and import it instead

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,27 +1,14 @@
 #!/usr/bin/env python3
 """A module that handle the base for geometric shapes"""
 
+BaseGeometry = __import__('base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """Class that represente the base for geometric shapes"""
-
-    def area(self):
-        """Calculate the area of the geometric shapes"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name="", value=1):
-        """Check if the value is an integer or not"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        return True
 
 class Rectangle(BaseGeometry):
     """Class that represente a square"""
     def __init__(self, width=1, height=1):
         """Constructor"""
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
