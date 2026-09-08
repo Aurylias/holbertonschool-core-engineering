ca10ade8a51bd7180ed82c54adff335427d3ec5e
Aurylias <brycevermorel@gmail.com>
2026-09-08 11:22:16 +0200

    fix: add self as argument in __str__

--- python_oop/inheritance/2-square.py
+++ python_oop/inheritance/2-square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
 
-    def __str__():
+    def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
 
     def area(self):
