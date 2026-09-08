f5dee7f1c2b22b250c0cc86bd08eb3cd6b1aea27
Aurylias <brycevermorel@gmail.com>
2026-09-08 10:51:10 +0200

    fix: add self in all method prototype

--- python_oop/inheritance/2-rectangle.py
+++ python_oop/inheritance/2-rectangle.py
@@ -13,9 +13,9 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__height = height   
 
-    def __str__():
+    def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
-    def area():
+    def area(self):
         """Calculate the area of the rectangle"""
         return self.__width * self.__height
