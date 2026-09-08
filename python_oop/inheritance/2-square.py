fbf97e4dac08f3bf0217327b5613932875654f64
Aurylias <brycevermorel@gmail.com>
2026-09-08 11:21:12 +0200

    Initial commit

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A module that handle the base for geometric shapes"""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that represent a square"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__():
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """Calculate the area of the square"""
+        return self.__size * self.__size
