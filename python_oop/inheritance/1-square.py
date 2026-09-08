8eaeb5e2912f71cdbef3cb9d410a81c66388420b
Aurylias <brycevermorel@gmail.com>
2026-09-08 11:19:18 +0200

    fix: add self as argument in area

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -10,6 +10,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
 
-    def area():
+    def area(self):
         """Calculate the area of the square"""
         return self.__size * self.__size
