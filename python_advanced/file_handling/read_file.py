ecba8a464f738509e88923a6be0cb1718a689790
Aurylias <brycevermorel@gmail.com>
2026-09-21 10:03:02 +0200

    fix: remove newline

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Print a whole file"""
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
-            print(line)
+            print(line.strip('\n'))
