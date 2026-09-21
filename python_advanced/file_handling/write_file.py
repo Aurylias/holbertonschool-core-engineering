1a2eb76894f968bbbc29e20243dfbc9476b234b7
Aurylias <brycevermorel@gmail.com>
2026-09-21 10:15:49 +0200

    fix: add return

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -6,3 +6,4 @@ def write_file(filename="", text=""):
     """Write a line in a file and create it if it doesn't exist"""
     with open(filename, "x", encoding="utf-8") as file:
         file.write(text)
+        return len(text)
